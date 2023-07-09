f5b6f856af43ef022eeb203b7145e02cdb55190e
Yueming Li <2771592l@student.gla.ac.uk>
2023-07-09 21:54:31 +0800

    n

--- src/main/java/com/ming/SpringbootApplication.java
+++ src/main/java/com/ming/SpringbootApplication.java
@@ -3,6 +3,7 @@ package com.ming;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
+//
 @RestController
 @SpringBootApplication
 public class SpringbootApplication {
