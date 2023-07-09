a17925f9880713ffa0db787d32ef56be4f341902
Yueming Li <2771592l@student.gla.ac.uk>
2023-07-09 21:49:13 +0800

    Initial commit

---
+++ src/main/java/com/ming/common/Constants.java
@@ -0,0 +1,15 @@
+package com.ming.common;
+
+public interface Constants {
+
+    String CODE_200 = "200";
+    String CODE_401 = "401";
+    String CODE_400 = "400";
+    String CODE_500 = "500";
+    String CODE_600 = "600";
+
+    String DICT_TYPE_ICON = "icon";
+
+    String FILES_KEY = "FILES_FRONT_ALL";
+
+}
